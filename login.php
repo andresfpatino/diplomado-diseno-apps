a79ab996eb107de225bff9ec5ae252abac8f18bc
Andrés Felipe <andresfelipepatino5@gmail.com>
2018-03-11 14:00:45 -0500

    Cambiamos el title

--- login.php
+++ login.php
@@ -11,7 +11,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Login | App registro de notas</title>
+	<title>App registro de notas | Diplomado Desarrollo apps FADP</title>
 	<meta name="description" content="">
 
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.2, user-scalable=no">
